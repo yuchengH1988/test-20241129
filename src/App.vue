492c8636a4f1babd78dc90760d3129e33df24340
CalvinHuang <yucheng.h.1988@gmail.com>
2024-11-29 14:15:26 +0800

    install gsap, add nav animate

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,29 @@
 </template>
 
 <script>
+import { onMounted } from 'vue'; 
+import { gsap } from 'gsap';
 import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
     NavBar
+  },
+  setup() {
+    onMounted(() => {
+      gsap.fromTo(
+        "nav",
+        { y: -100 },
+        {
+          y: 0,
+          duration: 1,
+          ease: "power3.out",
+        }
+      );
+    });
   }
-}
+} 
 </script>
 
 <style>
