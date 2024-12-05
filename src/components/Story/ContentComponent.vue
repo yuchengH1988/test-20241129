baca046ed24417efada507d5f6d20eafa6673561
CalvinHuang <yucheng.h.1988@gmail.com>
2024-12-05 10:49:09 +0800

    完成story-mobile

---
+++ src/components/Story/ContentComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="display-1 md:my-auto md:mx-0 my-0 mx-auto md:mb-0 mb-[313px]">
+    Story
+  </div>
+  <div class="text-center md:w-[773px] w-[312px] md:pl-[143px] md:pr-[156px] px-auto mt-0 mb-[47px] md:my-auto md:mx-0 mx-auto">
+    <h2 class="heading-2">Story</h2>
+    <p class="body-2 whitespace-normal mt-8">
+      Sit elit incididunt Lorem minim ea pariatur anim dolor ut veniam amet ad. Ipsum tempor commodo ullamco Lorem incididunt proident nisi nostrud dolore. Et labore minim laboris esse amet eu eu exercitation ea esse velit tempor culpa.
+    </p>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ContentComponent"
+  }
+</script>
--- src/components/Story/StoryComponent.vue
+++ src/components/Story/StoryComponent.vue
@@ -1,26 +1,8 @@
 <template>
-  <div id="Story" class="relative overflow-hidden h-[417px] border-t  bg-secondary-1 border-primary-2 text-primary-2 flex items-center">
-    <div ref="marquee" class="absolute flex whitespace-nowrap">
-      <div
-        class="display-0 my-auto">
-        Story
-      </div>
-      <div class="text-center w-[773px] pl-[143px] pr-[156px] my-auto">
-        <h2 class="heading-2">Story</h2>
-        <p class="body-2 whitespace-normal mt-8">
-          Sit elit incididunt Lorem minim ea pariatur anim dolor ut veniam amet ad. Ipsum tempor commodo ullamco Lorem incididunt proident nisi nostrud dolore. Et labore minim laboris esse amet eu eu exercitation ea esse velit tempor culpa.
-        </p>
-      </div>
-      <div
-        class="display-0 my-auto">
-        Story
-      </div>
-      <div class="text-center w-[773px] pl-[143px] pr-[156px] my-auto">
-        <h2 class="heading-2">Story</h2>
-        <p class="body-2 whitespace-normal mt-8">
-          Sit elit incididunt Lorem minim ea pariatur anim dolor ut veniam amet ad. Ipsum tempor commodo ullamco Lorem incididunt proident nisi nostrud dolore. Et labore minim laboris esse amet eu eu exercitation ea esse velit tempor culpa.
-        </p>
-      </div>
+  <div id="Story" class="relative justify-center overflow-hidden md:h-[417px] h-[680px] border-t bg-secondary-1 border-primary-2 text-primary-2 flex md:justify-start md:items-center">
+    <div ref="marquee" class="absolute flex md:flex-row flex-col whitespace-nowrap">
+      <ContentComponent/>
+      <ContentComponent/>
     </div>
     <div
       class="w-full flex items-center justify-center">
@@ -36,12 +18,17 @@
 <script>
 import { onMounted, ref, onUnmounted } from "vue";
 import { gsap } from "gsap";
+import ContentComponent from "./ContentComponent.vue";
 
 export default {
   name: "StoryComponent",
+  components: {
+    ContentComponent,
+  },
   setup() {
     const marquee = ref(null);
-    const rotatingUnicorn = ref(null)
+    const rotatingUnicorn = ref(null);
+    const isVertical = ref(window.innerWidth < 768);
 
     const onScroll = () => {
       const scrollY = window.scrollY;
@@ -52,20 +39,39 @@ export default {
         ease: "back.out(3)",
       });
     };
+    const onResize = () => {
+      isVertical.value = window.innerWidth < 768;
+      setupMarquee(); 
+    };
+    const setupMarquee = () => {
+      gsap.killTweensOf(marquee.value);
+      if (isVertical.value) {
+        gsap.to(marquee.value, {
+          y: -1335.4 ,
+          duration: 10,
+          repeat: -1,
+          ease: "linear",
+          modifiers: {
+            y: gsap.utils.unitize((y) => parseFloat(y) % (1335.4/2)),
+          },
+        });
+      } else {
+        gsap.to(marquee.value, {
+          x: -2848,
+          duration: 10,
+          repeat: -1,
+          ease: "linear",
+          modifiers: {
+            x: gsap.utils.unitize((x) => parseFloat(x) % 2848),
+          },
+        });
+      }
+    };
 
     onMounted(() => {
       window.addEventListener("scroll", onScroll);
-      const marqueeWidth = 2848;
-
-      gsap.to(marquee.value, {
-        x: -marqueeWidth,
-        duration: 10,
-        repeat: -1,
-        ease: "linear",
-        modifiers: {
-          x: gsap.utils.unitize((x) => parseFloat(x) % marqueeWidth /2),
-        },
-      });
+      window.addEventListener("resize", onResize);
+      setupMarquee();
     });
     onUnmounted(() => {
       window.removeEventListener("scroll", onScroll);
